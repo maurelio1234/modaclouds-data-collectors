268aadd0f0128ebf7ed068251527be4e57c4edc3
weikunwang <weikunwang89@gmail.com>
2014-05-08 10:25:56 +0100

    update the name of the data collector

--- src/main/java/imperial/modaclouds/monitoring/datacollectors/monitors/CostMonitor.java
+++ src/main/java/imperial/modaclouds/monitoring/datacollectors/monitors/CostMonitor.java
@@ -1,5 +1,5 @@
 /**
- * Copyright ${2014} Imperial
+ * Copyright ${year} deib-polimi
  * Contact: imperial <weikun.wang11@imperial.ac.uk>
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
